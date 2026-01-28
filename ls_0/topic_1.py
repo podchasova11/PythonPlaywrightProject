b6950c7cbcdf7fa295249c67cda319ffeb72d219
podchasova11 <pod4asova20@gmail.com>
2026-01-28 13:56:57 +0300

    add

--- ls_0/topic_1.py
+++ ls_0/topic_1.py
@@ -3,7 +3,7 @@ from playwright.sync_api import sync_playwright, Playwright, BrowserContext, Pag
 playwright = sync_playwright().start()
 browser = playwright.chromium.launch(headless=False)
 page = browser.new_page()
-page.goto("https://yahoo.cdm")
+page.goto("https://yahoo.com")
 page.wait_for_timeout(3000)
 browser.close()
 playwright.stop()
